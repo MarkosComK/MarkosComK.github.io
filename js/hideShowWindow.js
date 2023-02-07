9d3e8f6d243543f5a99028a637c5fbe6b6c3c6cf
MarkosComK <markoscomks@gmail.com>
2023-02-07 14:28:39 +0000

    added functionality to close the window

--- js/hideShowWindow.js
+++ js/hideShowWindow.js
@@ -1,7 +1,14 @@
-function changeDisplay(button, div){
+function changeDisplay(button, div, closeBtn){
     this.button = button
     this.div = div
+    this.closeBtn = closeBtn
+    closeBtn.addEventListener('click', close.bind(this))
     button.addEventListener('click', changeDisplay.bind(this))
+
+    function close(){
+        this.div.style.display = 'none'
+    }
+
     function changeDisplay(){
         if(this.div.style.display == 'none'){
             this.div.style.display = 'inline'
@@ -11,4 +18,4 @@ function changeDisplay(button, div){
     }
 }
 
-var calculator = new changeDisplay(document.querySelector('#calculatorBtn'), document.querySelector('.calculator'))
+var calculator = new changeDisplay(document.querySelector('#calculatorBtn'), document.querySelector('.calculator'), document.querySelector('.close-calculator'))

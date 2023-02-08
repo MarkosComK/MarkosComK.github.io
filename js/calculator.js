687d730faf0b93163656cb3c14f2fb091378828f
MarkosComK <markoscomks@gmail.com>
2023-02-08 15:38:42 +0000

    added multiply function

--- js/calculator.js
+++ js/calculator.js
@@ -1,7 +1,11 @@
 const items = document.querySelectorAll('#number')
 const sumBtn = document.querySelector('#sum')
+const subBtn = document.querySelector('#sub')
+const multiplyBtn = document.querySelector('#mult')
 const equalBtn = document.querySelector('#equal')
 const clearBtn = document.querySelector('#clear')
+
+
 
 const screen = document.querySelector('.result')
 const operationScreen = document.querySelector('.operation')
@@ -36,6 +40,22 @@ sumBtn.addEventListener('click', () => {
     }
 })
 
+// sub button
+subBtn.addEventListener('click', () => {
+    if(firstValue.length > 0){
+        opt = subBtn.dataset.opt
+        operationScreen.innerText = opt
+    }
+})
+
+// mult button
+multiplyBtn.addEventListener('click', () => {
+    if(firstValue.length > 0){
+        opt = multiplyBtn.dataset.opt
+        operationScreen.innerText = opt
+    }
+})
+
 
 // equal button
 equalBtn.addEventListener('click', () => {
@@ -43,6 +63,12 @@ equalBtn.addEventListener('click', () => {
     secondValue = Number(secondValue.join(''))
     if(opt == '+'){
         result = sum(firstValue, secondValue)
+    }
+    if(opt == '-'){
+        result = sub(firstValue, secondValue)
+    }
+    if(opt == 'x'){
+        result = multiply(firstValue, secondValue)
     }
     operationPress = true
     if(operationPress){
@@ -66,6 +92,13 @@ clearBtn.addEventListener('click', () => {
 
 // calculator functions 
 function sum(n1, n2){
-    return n1+n2
+    return n1 + n2
 }
 
+function sub(n1, n2){
+    return n1 - n2
+}
+
+function multiply(n1, n2){
+    return n1 * n2
+}

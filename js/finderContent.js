1513d034c213cccfe7efdfdcf61b1c319cf2aedc
MarkosComK <markoscomks@gmail.com>
2023-02-15 15:43:25 +0000

    added animations to finder profile load

---
+++ js/finderContent.js
@@ -0,0 +1,153 @@
+
+
+const aboutContent = `<div>
+<h1 class="hidden">Hello, my name is Markos and i am a front-end developer.</h1>
+<div class="profile-wrap hidden">
+    <img src="./imagens/me-art.JPG" alt="">
+    <div>
+        <p><strong>Markos Vinicius</strong></p>
+        <p>Web-developer from Brazil, based in Portugal</p>
+    </div>
+</div>
+<div class="text-wrap">
+    <p class="hidden">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus est et
+    sem aliquet, sit amet vulputate ante auctor. Aenean eleifend enim vitae purus 
+    mollis, et suscipit eros elementum. Duis ornare, eros in ullamcorper sollicitudin, 
+    dolor lorem accumsan tellus, at pellentesque quam est vitae libero.
+    </p>
+    <p class="hidden">
+        Proin ultricies neque in ante pellentesque luctus. Vestibulum venenatis mi vel 
+        orci vulputate, vel faucibus tortor pharetra. Vivamus hendrerit libero vel eros 
+        tempor, a posuere massa mollis. Morbi facilisis mauris ut lacus ullamcorper 
+        consequat.
+    </p>
+    <p class="hidden">
+        Donec venenatis augue ut magna suscipit, non suscipit arcu tristique. Phasellus 
+        vel diam et enim interdum iaculis. Sed tempor, eros vel suscipit fringilla, nunc 
+        nibh euismod orci, sed varius ante leo ut lorem. Fusce nec posuere mi
+    </p>
+    <p class="hidden">
+        Pellentesque facilisis, lectus quis pulvinar pretium, metus orci euismod lacus, 
+        sed volutpat magna nisl at odio. Fusce aliquet laoreet felis eget commodo. Sed 
+        pharetra odio id leo mollis, a congue lorem vehicula. Nunc ut dui eu leo 
+        tristique commodo sit amet vel velit.
+    </p>
+    <p class="hidden">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus est et
+    sem aliquet, sit amet vulputate ante auctor. Aenean eleifend enim vitae purus 
+    mollis, et suscipit eros elementum. Duis ornare, eros in ullamcorper sollicitudin, 
+    dolor lorem accumsan tellus, at pellentesque quam est vitae libero.
+    </p>
+    <p class="hidden">
+        Proin ultricies neque in ante pellentesque luctus. Vestibulum venenatis mi vel 
+        orci vulputate, vel faucibus tortor pharetra. Vivamus hendrerit libero vel eros 
+        tempor, a posuere massa mollis. Morbi facilisis mauris ut lacus ullamcorper 
+        consequat.
+    </p>
+    <p class="hidden">
+        Donec venenatis augue ut magna suscipit, non suscipit arcu tristique. Phasellus 
+        vel diam et enim interdum iaculis. Sed tempor, eros vel suscipit fringilla, nunc 
+        nibh euismod orci, sed varius ante leo ut lorem. Fusce nec posuere mi
+    </p>
+    <p class="hidden">
+        Pellentesque facilisis, lectus quis pulvinar pretium, metus orci euismod lacus, 
+        sed volutpat magna nisl at odio. Fusce aliquet laoreet felis eget commodo. Sed 
+        pharetra odio id leo mollis, a congue lorem vehicula. Nunc ut dui eu leo 
+        tristique commodo sit amet vel velit.
+    </p>
+</div>
+</div>`
+const aboutContent2 = `<div>
+<h1 class="show-content">Hello, my name is Markos and i am a front-end developer.</h1>
+<div class="profile-wrap show-content">
+    <img src="./imagens/me-art.JPG" alt="">
+    <div>
+        <p><strong>Markos Vinicius</strong></p>
+        <p>Web-developer from Brazil, based in Portugal</p>
+    </div>
+</div>
+<div class="text-wrap">
+    <p class="show-content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus est et
+    sem aliquet, sit amet vulputate ante auctor. Aenean eleifend enim vitae purus 
+    mollis, et suscipit eros elementum. Duis ornare, eros in ullamcorper sollicitudin, 
+    dolor lorem accumsan tellus, at pellentesque quam est vitae libero.
+    </p>
+    <p class="show-content">
+        Proin ultricies neque in ante pellentesque luctus. Vestibulum venenatis mi vel 
+        orci vulputate, vel faucibus tortor pharetra. Vivamus hendrerit libero vel eros 
+        tempor, a posuere massa mollis. Morbi facilisis mauris ut lacus ullamcorper 
+        consequat.
+    </p>
+    <p class="show-content">
+        Donec venenatis augue ut magna suscipit, non suscipit arcu tristique. Phasellus 
+        vel diam et enim interdum iaculis. Sed tempor, eros vel suscipit fringilla, nunc 
+        nibh euismod orci, sed varius ante leo ut lorem. Fusce nec posuere mi
+    </p>
+    <p class="show-content">
+        Pellentesque facilisis, lectus quis pulvinar pretium, metus orci euismod lacus, 
+        sed volutpat magna nisl at odio. Fusce aliquet laoreet felis eget commodo. Sed 
+        pharetra odio id leo mollis, a congue lorem vehicula. Nunc ut dui eu leo 
+        tristique commodo sit amet vel velit.
+    </p>
+    <p class="show-content">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed faucibus est et
+    sem aliquet, sit amet vulputate ante auctor. Aenean eleifend enim vitae purus 
+    mollis, et suscipit eros elementum. Duis ornare, eros in ullamcorper sollicitudin, 
+    dolor lorem accumsan tellus, at pellentesque quam est vitae libero.
+    </p>
+    <p class="show-content">
+        Proin ultricies neque in ante pellentesque luctus. Vestibulum venenatis mi vel 
+        orci vulputate, vel faucibus tortor pharetra. Vivamus hendrerit libero vel eros 
+        tempor, a posuere massa mollis. Morbi facilisis mauris ut lacus ullamcorper 
+        consequat.
+    </p>
+    <p class="show-content">
+        Donec venenatis augue ut magna suscipit, non suscipit arcu tristique. Phasellus 
+        vel diam et enim interdum iaculis. Sed tempor, eros vel suscipit fringilla, nunc 
+        nibh euismod orci, sed varius ante leo ut lorem. Fusce nec posuere mi
+    </p>
+    <p class="show-content">
+        Pellentesque facilisis, lectus quis pulvinar pretium, metus orci euismod lacus, 
+        sed volutpat magna nisl at odio. Fusce aliquet laoreet felis eget commodo. Sed 
+        pharetra odio id leo mollis, a congue lorem vehicula. Nunc ut dui eu leo 
+        tristique commodo sit amet vel velit.
+    </p>
+</div>
+</div>`
+
+
+const workContent = `<div>Ola mundo</div>`
+
+
+const workBtn = document.querySelector('#work-btn')
+const aboutBtn = document.querySelector('#about-btn')
+
+
+let about =  document.querySelector('.about')
+about.innerHTML = aboutContent
+
+
+aboutBtn.addEventListener('click', () => {
+    about.innerHTML = aboutContent2
+    
+})
+
+workBtn.addEventListener('click', () => {
+    about.innerHTML = workContent
+})
+
+// finder content scroll animation
+
+const hiddenContent = document.querySelectorAll('.hidden')
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show-content')
+        } 
+    })
+})
+
+hiddenContent.forEach((el) => {observer.observe(el)})
